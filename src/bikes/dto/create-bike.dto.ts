fc67ba87579de4bc0e2a2c1a11eac7d01a0e0436
Lbarrerob <l.barrerob@uniandes.edu.co>
2026-04-20 17:23:38 -0500

    TODOs de bike

--- src/bikes/dto/create-bike.dto.ts
+++ src/bikes/dto/create-bike.dto.ts
@@ -2,16 +2,18 @@ import { IsEnum, IsNumber, IsPositive, IsString, MinLength } from 'class-validat
 import { BikeType } from '../entities/bike.entity';
 
 export class CreateBikeDto {
-  // TODO: Add @IsString() and @MinLength(2)
+  @IsString()
+  @MinLength(2)
   marca: string;
 
-  // TODO: Add @IsEnum(BikeType)
+  @IsEnum(BikeType)
   // Valid values are in the BikeType enum: mountain, road, city, electric
   tipo: BikeType;
 
-  // TODO: Add @IsNumber() and @IsPositive()
+  @IsNumber()
+  @IsPositive()
   velocidades: number;
 
-  // TODO: Add @IsString()
+  @IsString()
   descripcion: string;
 }
