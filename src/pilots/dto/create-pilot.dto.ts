03ee60de3cccc5e3a0494b2a1cafc28be9f01b82
Lbarrerob <l.barrerob@uniandes.edu.co>
2026-04-20 18:50:35 -0500

    pilots

---
+++ src/pilots/dto/create-pilot.dto.ts
@@ -0,0 +1,21 @@
+import { IsBoolean, IsNumber, IsPositive, IsString, Min } from 'class-validator';
+
+export class CreatePilotDto {
+  @IsString()
+  nombre: string;
+
+  @IsString()
+  escuderia: string;
+
+  @IsNumber()
+  @IsPositive()
+  numero: number;
+
+  @IsBoolean()
+  activo: boolean;
+
+  @IsNumber()
+  @IsPositive()
+  @Min(0)
+  campeonatos: number;
+}
